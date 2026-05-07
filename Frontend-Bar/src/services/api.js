e37c9a83005984076487bef33c18b6a45ccfb27f
Arita <aritalvarez03@gmail.com>
2026-05-08 00:06:03 +0200

    cambios de fetch

--- Frontend-Bar/src/services/api.js
+++ Frontend-Bar/src/services/api.js
@@ -1,6 +1,6 @@
 // Archivo para manejar las llamadas a la API del backend y obtener los datos de los platos
 export const getPlatos = async () => {
-  const res = await fetch("https://the-colors-gastroteka.onrender.com");
+  const res = await fetch("https://the-colors-gastroteka.onrender.com/platos");
 
   if (!res.ok) {
     throw new Error("Error fetching menu");
