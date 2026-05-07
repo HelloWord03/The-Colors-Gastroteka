9d87ca3e7e3e56177354b9797a42a9380226cf1e
Arita <aritalvarez03@gmail.com>
2026-05-08 00:03:32 +0200

    deploy en render

--- Frontend-Bar/src/services/api.js
+++ Frontend-Bar/src/services/api.js
@@ -1,6 +1,6 @@
 // Archivo para manejar las llamadas a la API del backend y obtener los datos de los platos
 export const getPlatos = async () => {
-  const res = await fetch("http://localhost:3000/platos");
+  const res = await fetch("https://the-colors-gastroteka.onrender.com");
 
   if (!res.ok) {
     throw new Error("Error fetching menu");
