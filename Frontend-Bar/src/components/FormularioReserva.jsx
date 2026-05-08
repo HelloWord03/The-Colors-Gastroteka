063a1dbe90656dbcd072e88eb64a0b9deadb5372
Arita <125996674+HelloWord03@users.noreply.github.com>
2026-05-08 11:18:12 +0200

    prueba emojis

--- Frontend-Bar/src/components/FormularioReserva.jsx
+++ Frontend-Bar/src/components/FormularioReserva.jsx
@@ -34,11 +34,11 @@ export default function FormularioReserva() {
     const telefono = "688758342";
 
     const emoji = {
-      plato:   String.fromCodePoint(\uD83C\uDF7D\uFE0F),    // 🍽️
-      persona: String.fromCodePoint(0x1F464),               // 👤
-      email:   String.fromCodePoint(0x1F4E7),               // 📧
-      nota:    String.fromCodePoint(0x1F4DD),               // 📝
-    };
+        plato:   String.fromCodePoint(0x1F37D, 0xFE0F), // 🍽️
+        persona: String.fromCodePoint(0x1F464),          // 👤
+        email:   String.fromCodePoint(0x1F4E7),          // 📧
+        nota:    String.fromCodePoint(0x1F4DD),          // 📝
+  };
 
     // Códigos de emojis para evitar problemas de encoding UTF-8
     const texto =       
