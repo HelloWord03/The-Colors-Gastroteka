594a0121b3037bc9eb4128d608e7b7906141142c
Arita <aritalvarez03@gmail.com>
2026-07-13 20:54:16 +0200

    cambios de idioma

--- Frontend-Bar/js/cambiar-idioma.js
+++ Frontend-Bar/js/cambiar-idioma.js
@@ -1,9 +1,11 @@
 // cambiar idioma de la página al hacer clic en el botón de idioma
-const botonIdioma = document.getElementById("cambiar-idioma");
-const idiomaActual = localStorage.getItem("idioma") || "es"; // idioma por defecto es español
-
-// Función para cambiar el idioma   
-function cambiarIdioma() {
-  const nuevoIdioma = idiomaActual === "es" ? "en" : "es";  
-    localStorage.setItem("idioma", nuevoIdioma);
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    {
+      pageLanguage: "es",
+      includedLanguages: "es,en,fr,it,de,pt,zh-CN,ja,ko,ar,ru",
+      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    },
+    "google_translate_element"
+  );
 }
